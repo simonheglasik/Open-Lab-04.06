7f14c86d8e594a4f8291511a3a474cc477545a3f
simonheglasik <simonheglasik@gmail.com>
2019-11-07 09:56:16 +0100

    04.06

--- Open-Lab-04.06/Numbers.cs
+++ Open-Lab-04.06/Numbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Open_Lab_04._06
 {
@@ -6,7 +7,15 @@ namespace Open_Lab_04._06
     {
         public int[] NoOdds(int[] numbers)
         {
-            throw new NotImplementedException();
+            List<int> a = new List<int> { };
+            foreach (int i in numbers)
+            {
+                if (i % 2 == 0)
+                {
+                      a.Add(i) ;
+                }
+            }
+            return a.ToArray();
         }
     }
 }
